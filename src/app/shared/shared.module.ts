72b89e9dd5d52b34d6d9549b53e7ea4b0f0d1b53
DeathApex <golden.highlife@hotmail.com>
2022-07-18 17:35:46 -0500

    maqueta template form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
 
@@ -12,7 +14,8 @@ import { SidemenuComponent } from './sidemenu/sidemenu.component';
     SidemenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
